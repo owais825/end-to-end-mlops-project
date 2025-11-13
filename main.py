aacca78b06c63d7536802b4980c824be69879350
Owais Wani <owais825@gmail.com>
2025-11-13 15:03:04 +0530

    model training done

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from mlops_project import logger
 from mlops_project.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from mlops_project.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
-
+from mlops_project.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 STAGE_NAME = "Data Ingestion Stage"
 
 try:
@@ -19,6 +19,16 @@ STAGE_NAME = "Data Validation Stage"
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
     obj = DataValidationTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Data Transformation Stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataTransformationTrainingPipeline()
     obj.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
