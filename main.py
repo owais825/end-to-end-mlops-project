06243f50854ce135da39ccc5c05ed6ed5f94eb84
Owais Wani <owais825@gmail.com>
2025-11-13 13:29:07 +0530

    Data Validation added

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from mlops_project import logger
 from mlops_project.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-
+from mlops_project.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 
@@ -9,6 +9,18 @@ try:
     obj = DataIngestionTrainingPipeline()
     obj.main()
     logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<<\n\nx======x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Data Validation Stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataValidationTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
     logger.exception(e)
     raise e
--- src/mlops_project/config/configuration.py
+++ src/mlops_project/config/configuration.py
@@ -1,9 +1,9 @@
 from mlops_project.constants import *
 from mlops_project.utils.common import read_yaml, create_directories
-from mlops_project.entity.config_entity import DataIngestionConfig
+from mlops_project.entity.config_entity import (DataIngestionConfig, DataValidationConfig) 
 class ConfigurationManager:
     def __init__(self,
-                 config_filepath = CONFIF_FILE_PATH,
+                 config_filepath = CONFIG_FILE_PATH,
                  params_filepath = PARAMS_FILE_PATH,
                  schema_filepath = SCHEMA_FILE_PATH):
         self.config = read_yaml(config_filepath)
@@ -26,5 +26,20 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+    
+    def get_data_validation_config(self) -> DataValidationConfig:
+        config = self.config.data_validation
+        schema = self.schema.COLUMNS
+
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+            root_dir=config.root_dir,
+            STATUS_FILE=config.STATUS_FILE,
+            unzip_data_dir = config.unzip_data_dir,
+            all_schema=schema,
+        )
+
+        return data_validation_config
         
         
